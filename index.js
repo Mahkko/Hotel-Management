587d6d35c57b387763e77dedb11fb4caba30fbc4
nnatasaa <natasa.stjepiccosic@gmail.com>
2024-12-16 13:07:43 +0100

    class Room added

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ class Hotel {
     name;
     static rooms = [];
     static users = [];
+    constructor (name) {
+        this.name = name;
+    }
 }
+
+class Room {
+    roomID = 1;
+    roomType; // single, double, apartment
+    isOccupied;
+    currentOccupant;
+    constructor(roomID, roomType) {
+        this.roomID = roomID;
+        this.roomType = roomType;
+        this.isOccupied = false;
+        Hotel.rooms.push(this);
+    }
+}
+
+//changes:
+// added constructor to the class Hotel
+// created class Room
